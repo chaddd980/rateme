0f41dac50aa21040b762644cf5266e9bf6a18294
chadd williams <chadd980@hotmail.com>
2017-03-07 23:55:38 -0500

    added login and signup page

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,15 @@
 module.exports = (app) => {
 
   app.get('/', (req, res, next) => {
-    res.render('index');
+    res.render('index', {title: "Index || RateMe"});
   });
 
   app.get('/signup', (req, res) => {
-    res.render('user/signup')
+    res.render('user/signup', {title: 'Sign up || RateMe'});
   });
-  
+
+  app.get('/login', (req, res) => {
+    res.render('user/login', {title: 'Login || RateMe'});
+  });
+
 }
